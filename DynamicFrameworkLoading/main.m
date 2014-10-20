177fb5ca1011f6405b5356b1f98f84d0d763a23d
sbausis <sbausis@gmx.net>
2014-10-20 16:32:43 +0200

    use SBJson Submodule

--- DynamicFrameworkLoading/main.m
+++ DynamicFrameworkLoading/main.m
@@ -8,10 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+#include <dlfcn.h>
+#include <SBJson/SBJson.h>
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        void* framework_handle = dlopen("SBJson.framework/SBJson", RTLD_LAZY);
+        if (framework_handle) {
+            NSLog(@"Loaded SBJson.framework ...");
+            
+            NSDictionary* dict = [NSDictionary dictionaryWithDictionary:[[NSMutableDictionary alloc] initWithObjectsAndKeys:@"Hello", @"World", nil]];
+            NSLog(@"DICT : %@", dict);
+            
+            NSString* json = [dict JSONRepresentation];
+            NSLog(@"JSON : %@", json);
+            
+            NSDictionary* newDict = [json JSONValue];
+            NSLog(@"DICT : %@", newDict);
+        }
+        
     }
     return 0;
 }
