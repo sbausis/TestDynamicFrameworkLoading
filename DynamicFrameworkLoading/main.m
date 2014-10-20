971aaa556314c6fe348641637aab4354d78c3c55
sbausis <sbausis@gmx.net>
2014-10-20 12:39:32 +0200

    Initial Commit

---
+++ DynamicFrameworkLoading/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DynamicFrameworkLoading
+//
+//  Created by Simon Pascal Baur on 20/10/14.
+//  Copyright (c) 2014 Simon Pascal Baur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
